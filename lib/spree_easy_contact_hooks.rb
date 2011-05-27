49d84b347de5f87dfd0ba44996701fbb992654c5
pierre <pba@netaplan.com>
2011-05-27 22:36:25 +0200

    add compatibily with last version

--- lib/spree_easy_contact_hooks.rb
+++ lib/spree_easy_contact_hooks.rb
@@ -1,11 +1,15 @@
 class SpreeEasyContactHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
-  insert_after :admin_tabs do
-    %(<%= tab(:contacts) %>)
-  end
-  
-  insert_after :admin_configurations_menu do
-    "<%= configurations_menu_item(I18n.t('topics'), admin_topics_path, I18n.t('topics_description')) %>"
-  end
+  Deface::Override.new(:virtual_path => "layouts/admin",
+                       :name => "converted_admin_tabs_351915426",
+                       :insert_bottom  => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                       :text => "<%= tab(:contacts) %>",
+                       :disabled => false)
+
+  Deface::Override.new(:virtual_path => "admin/configurations/index",
+                       :name => "converted_admin_configurations_menu_586883267",
+                       :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                       :text => "<%= configurations_sidebar_menu_item t('topics'), admin_topics_path, t('topics_description') %>",
+                       :disabled => false)
   
 end
