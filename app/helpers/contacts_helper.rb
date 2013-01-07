1c6a98f5f252ff7c474f1b86c514b29fb00b3a5d
Christopher Maujean <cmaujean@gmail.com>
2013-01-06 18:11:19 -0800

    List all orders in the new contact dropdown, ordered by most recently updated

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,7 +1,5 @@
 module ContactsHelper
   def options_for_current_orders
-    options_for_select([''] + current_user.orders.unfinished.map { |o|
-      o.number
-    })
+    options_for_select([''] + current_user.orders.sort_by{|o| o.updated_at }.reverse.map { |o| o.number })
   end
 end
