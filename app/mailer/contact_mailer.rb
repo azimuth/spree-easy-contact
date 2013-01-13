282eba04227b260547488483efa99289ab011c8d
Christopher Maujean <cmaujean@gmail.com>
2013-01-13 15:40:07 -0800

    fix mailer to use mailer templates.

--- app/mailer/contact_mailer.rb
+++ app/mailer/contact_mailer.rb
@@ -1,16 +1,16 @@
 class ContactMailer < ActionMailer::Base
   helper "spree/base"
 
+  # Mails a message to the topic recipients list (internal)
   def message_email(message)
     subject = "#{Spree::Config[:site_name]} - #{t('message_from')} #{message.email}"
 
     @message = message
     mail(:to => message.conversation.topic.email, :subject => subject, :reply_to => message.email)
   end
+
+  # Mails a message to the customer, acknowledging the contact
   def message_received_email(message)
-    mail(:to => message.email, :subject => Spree::Config[:auto_response_email_subject], :reply_to => Spree::Config[:auto_response_email_reply_to]) do |format|
-      format.html { render :text => Spree::Config[:auto_response_email_body_html] }
-      format.text { render :text => Spree::Config[:auto_response_email_body_text] }
-    end
+    mail(:to => message.email, :subject => Spree::Config[:auto_response_email_subject], :reply_to => Spree::Config[:auto_response_email_reply_to])
   end
 end
