f66a8d4104fbf115cacea64de2a6773b7d523532
Day Davis Waterbury <day@azimuthonline.com>
2013-03-05 15:06:12 -0800

    We should toss the conversation if initial contact creation fails because otherwise the admin chokes up on the empty conversation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,6 +20,9 @@ class ContactsController < Spree::BaseController
         ContactMailer.message_received_email(@contact).deliver
         format.html { redirect_to(root_path, :notice => t("message_sent")) }
       else
+        if @conversation.contacts.count == 0
+          @conversation.destroy
+        end
         format.html { render :action => "new" }
       end
     end
